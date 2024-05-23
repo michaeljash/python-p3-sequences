0b341a44ed0cb4072988c40ef37718880639bdf0
jashonmichael <jashon.michael@student.moringaschool.com>
2024-05-23 16:16:55 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python3
+def print_fibonacci(n):
+    fibonacci_sequence = [0, 1]
+    
+    if n <= 0:
+        print([])
+        return
+    
 
-def print_fibonacci(length):
-    pass
+    if n == 1:
+        print([0])
+        return
+    
+    while len(fibonacci_sequence) < n:
+        next_fib = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_fib)
+    
+    print(fibonacci_sequence[:n])
+
+print_fibonacci(9)
